88957997bbd0b16d25b3ba3910b7355b11abc62e
hua-lun <chuahualun@gmail.com>
2019-10-15 19:47:01 +0800

    Add Statistics function

---
+++ src/main/java/seedu/address/logic/commands/StatisticsCommand.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands;
+import org.apache.commons.math3.stat.StatUtils;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Adds a person to Duke Cooks.
+ */
+public class StatisticsCommand extends Command {
+
+    public static final String COMMAND_WORD = "stat";
+
+    public static final String MESSAGE_SUCCESS = "Show statistics ";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
